6689ed0b85385149aa6f410f10e817919bd4af23
Maxwell Vance <max.vance@gmail.com>
2014-03-21 13:21:25 -0500

    two more animations, more styling

--- app/static/js/source/animation.js
+++ app/static/js/source/animation.js
@@ -10,6 +10,8 @@
     $('#grid').on('click', '.card', flipCard);
     $('#flipAll').click(flipAll);
     $('#unflipAll').click(unflipAll);
+    $('#flipRow').click(flipRow);
+    $('#flipColumn').click(flipColumn);
   }
 
   function flipCard(id, unflip, fn){
@@ -30,26 +32,60 @@
       }
     });
   }
-
-  function flipAll(){
-    var cards = $('.card');
+  function flipMulti(cards){
     for (var x = 0;x < cards.length;x++){
       if ($(cards[x]).css('display') === 'block'){ //is it unflipped?
         flipCard(cards[x], false, function(){});
       }
     }
-    event.preventDefault();
   }
 
-  function unflipAll(){
-    var cards = $('.card');
+  function unflipMulti(cards){
     for (var x = 0;x < cards.length;x++){
       if ($(cards[x]).css('display') === 'none'){ //is it flipped?
         flipCard(cards[x], true, function(){});
       }
     }
+  }
+
+  function flipAll(){
+    var cards = $('.card');
+    flipMulti(cards);
     event.preventDefault();
   }
+
+  function unflipAll(){
+    var cards = $('.card');
+    unflipMulti(cards);
+    event.preventDefault();
+  }
+
+  function flipRow(row){
+    if (typeof row === 'object'){
+      row = parseInt($('#row-to-flip').val());
+    }
+    if (row > 5 || row < 1){
+      alert('Invalid row.'); //rows are one-indexed for the purpose of user input
+    } else {
+      row = row - 1;
+      var cards = $('#row'+row).children().children();
+      flipMulti(cards);
+    }
+  }
+
+  function flipColumn(column){
+    if (typeof column === 'object'){
+      column = parseInt($('#column-to-flip').val());
+    }
+    if (column > 5 || column < 1){
+      alert('Invalid column.');
+    } else {
+      column = column - 5; //5n -1, -2, -3, -4, etc.
+      var cards = $('.space:nth-child(5n'+column+')').children();
+      flipMulti(cards);
+    }
+  }
+
 
 })();
 
